2b7027ed5f56a7e52430c0acb1fa504194b4a3d0
Edward Badel <edwardcbadel@gmail.com>
2025-06-12 23:04:35 -0400

    Remove my name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,12 +39,11 @@ export default function RootLayout({
         <main className="min-h-screen" id="site-main-content">
             {children}
         </main>
-        <footer className="bg-stone-800 p-4 text-center space-y-1 text-white-muted text-sm pt-8 font-sans">
+        <footer className="space-y-1 bg-stone-800 p-4 pt-8 font-sans text-white-muted text-sm text-center">
             <div>Colony Irrigation - P.O. Box 6011 Plymouth, MI 48170</div>
             <div><Email address="anthony@colonyirrigation.com" >anthony@colonyirrigation.com</Email> - <PhoneCall number={7343985837}  className="underline">(734) 398-5837</PhoneCall></div>
             <div>Copyright © {new Date().getFullYear()} | All rights, images, & text reserved</div>
             <div><Link href="https://www.freeprivacypolicy.com/live/e8071626-51ea-4638-b867-5e7e12162879" className="underline">Privacy Policy</Link></div>
-            <div>Site built by <Link href="https://www.linkedin.com/in/edward-badel-7472a02a3/"  className="underline">Edward Badel</Link></div>
         </footer>
       </body>
     </html>
